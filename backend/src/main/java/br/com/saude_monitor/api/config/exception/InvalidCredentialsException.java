8c98018dbdf1b63134afcf2e271c6200d76aaba1
Gabriel Vogado <gabriel.vogado@gmail.com>
2026-03-21 21:27:34 -0300

    chore: update .gitignore to include local Gradle configurations and essential project files

--- backend/src/main/java/br/com/saude_monitor/api/config/exception/InvalidCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package br.com.saude_monitor.api.config.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-	public InvalidCredentialsException(String message) {
-		super(message);
-	}
-}
-

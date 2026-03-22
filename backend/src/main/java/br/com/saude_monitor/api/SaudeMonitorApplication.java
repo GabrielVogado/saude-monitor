bfb2433e85f087ea013901af2bd05efb813056fc
Gabriel Vogado <gabriel.vogado@gmail.com>
2026-03-21 22:05:15 -0300

    feat: enhance login response and improve validation error handling

--- backend/src/main/java/br/com/saude_monitor/api/SaudeMonitorApplication.java
+++ backend/src/main/java/br/com/saude_monitor/api/SaudeMonitorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SaudeMonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	static void main(String[] args) {
+		SpringApplication.run(SaudeMonitorApplication.class, args);
 	}
 
 }
